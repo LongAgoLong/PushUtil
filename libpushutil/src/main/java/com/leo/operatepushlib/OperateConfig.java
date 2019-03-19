e9332c5a2b0ada4f97d5beca109d4ca1038d586f
LEO <448740656@qq.com>
2019-03-19 17:58:19 +0800

    Update OperateConfig.java

--- libpushutil/src/main/java/com/leo/operatepushlib/OperateConfig.java
+++ libpushutil/src/main/java/com/leo/operatepushlib/OperateConfig.java
@@ -3,7 +3,6 @@ package com.leo.operatepushlib;
 public class OperateConfig {
 
     //用户在华为开发者联盟申请的appId和appSecret（会员中心->我的产品，点击产品对应的Push服务，点击“移动应用详情”获取）
-    public static final String HUAWEI_APP_ID = "";
     public static final String HUAWEI_TOKEN_URL = "https://login.cloud.huawei.com/oauth2/v2/token"; //获取认证Token的URL
     public static final String HUAWEI_API_URL = "https://api.push.hicloud.com/pushsend.do"; //应用级消息下发API
 }
